d3292b9064508d4e9747369faf4c75c7bd222aa2
artful-gopher <yugam.sharma@yahoo.com>
2020-04-25 12:44:19 -0700

    Test 1 - Testing feature to send reminder for any TO

--- src/ask-for-TO.py
+++ src/ask-for-TO.py
@@ -1,10 +1,18 @@
 import os
 import slack
 import subprocess
-import csv
 from datetime import datetime, timedelta
 
 # 1 Ask for any TO?
 client = slack.WebClient(token=os.environ['SLACK_API_TOKEN'], ssl=False)
 
-response = client.chat_postMessage(channel=os.environ['CHANNEL_ID'], text=":minion_yah: Hi "+os.environ['SLACK_EAST_USERGROUP']+ " Any TO? Please update the tool with your TO case: https://commit.cfapps.io/turnover and add TO notes to the case. :minion_yah:")
+time_now = str(subprocess.check_output(["date", "+%H"]), 'utf-8') # bytes returned here contains a '\n'
+print(time_now.rstrip('\n'))
+
+if(time_now.rstrip('\n') == '12'):
+    SLACK_USERGROUP = os.environ['SLACK_EAST_USERGROUP']
+
+if(time_now.rstrip('\n') == '16'):
+    SLACK_USERGROUP = os.environ['SLACK_WEST_USERGROUP']
+
+response = client.chat_postMessage(channel=os.environ['CHANNEL_ID'], text=":minion_yah: Hi "+SLACK_USERGROUP+ " Any TO? Please update the tool with your TO case: https://commit.cfapps.io/turnover and begin reaching out to your pair :blob_council:")
